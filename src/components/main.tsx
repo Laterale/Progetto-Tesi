4fdc84f099b4a993a4adf501e63324dc12128889
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2025-09-21 17:20:33 +0200

    map page changes

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -40,7 +40,7 @@ const Main = () => {
   return (
     <>
       {/* <BreakpointDisplay /> */}
-      <div className="fixed inset-0 flex bg-sky-200 pointer-events-auto">
+      <div className="fixed inset-0 flex bg-[#46b1e2] pointer-events-auto">
         <AnimatePresence mode="wait">
           {div0InView && <MenuBackground key="div0-bg"/>}
           {div1InView && <EuMapBackground key="div1-bg" />}
--- src/components/sections/section1-eu-map.tsx
+++ src/components/sections/section1-eu-map.tsx
@@ -2,13 +2,13 @@ import { motion } from "framer-motion"
 import PageSwitcher, { pageIds } from "~/components/page-switcher"
 import { useDictionary } from "~/lib/i18n"
 import {useRef, useCallback} from 'react';
-import Map, {MapRef} from 'react-map-gl/mapbox';
+import Map, {MapRef, Marker} from 'react-map-gl/mapbox';
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoibGF0ZXJhbGUiLCJhIjoiY21mOGc4a2FzMG96eDJqczY2bjZ2ZWJ3NiJ9.edTo59w8IaMW2MhiESU7gw';
 const initialViewState = {
-  latitude: 37.6666636,
-  longitude: -0.77333,
-  zoom: 1,
+  latitude: 43.6666636,
+  longitude: 5.77333,
+  zoom: 0,
   bearing: 0,
   pitch: 0
 };
@@ -23,62 +23,30 @@ const LAGOONS = [
 export default function ControlPanel(props: { onSelectLagoon: (arg0: { lagoon: string; image: string; latitude: number; longitude: number; zoom:number}) => void; }) {
   return (
     <motion.div
-      className="p-2 border-4 rounded-l-xl border-r-0 bg-white border-white"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+    className="p-1 bg-transparent flex gap-2 w-full"
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
     >
-      <div className="grid gap-3">
-        {LAGOONS.map((lagoon) => (
-          <motion.button
-            key={lagoon.lagoon}
-            onClick={() => props.onSelectLagoon(lagoon)}
-            className="group relative overflow-hidden rounded-md transition-all"
-            whileHover={{ scale: 1.03 }}
-            whileTap={{ scale: 0.97 }}
-          >
-            <img
-              src={lagoon.image}
-              alt={lagoon.lagoon}
-              className="h-16 w-full object-cover"
-            />
-            <div className="absolute inset-0 flex items-end p-3">
-              <p className="text-white text-sm">{lagoon.lagoon}</p>
-            </div>
-          </motion.button>
-        ))}
-      </div>
+      {LAGOONS.map((lagoon) => (
+        <motion.button
+        key={lagoon.lagoon}
+        onClick={() => props.onSelectLagoon(lagoon)}
+        className="group relative overflow-hidden rounded-md transition-all h-20 w-1/4"
+        whileTap={{ scale: 0.97 }}
+        >
+          <img
+          src={lagoon.image}
+          alt={lagoon.lagoon}
+          className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 flex items-end p-2 bg-gradient-to-t from-black/60 to-transparent">
+          <p className="text-white text-xs">{lagoon.lagoon}</p>
+          </div>
+        </motion.button>
+      ))}
     </motion.div>
   );
-}
-
-const LagoonMap = () => {
-  const mapRef = useRef<MapRef | null>(null);
-  const onSelectLagoon = useCallback(
-    ({ longitude, latitude, zoom}: { longitude: number; latitude: number; zoom:number}) => {
-    mapRef.current?.flyTo({ center: [longitude, latitude], zoom: zoom,  duration: 10000 });
-  },
-  []
-);
-return(
-  <>
-  <Map
-    ref={mapRef}
-    initialViewState={initialViewState}
-    mapStyle="mapbox://styles/laterale/cmfbg0o7t005601quciskd6r5"
-    mapboxAccessToken={MAPBOX_TOKEN}
-    dragPan={false}
-    dragRotate={false}
-    scrollZoom={false}
-    doubleClickZoom={false}
-    touchZoomRotate={false}
-    keyboard={false} 
-  />
-  <div className='absolute top-52 right-0'>
-    <ControlPanel onSelectLagoon={onSelectLagoon} />
-  </div>
-  </>
-)
 }
 
 export const EuMapBackground = () => {
@@ -96,12 +64,43 @@ export const EuMapBackground = () => {
 
 export const EuMapContent = () => {
   const { euMap: dictionary } = useDictionary()
+  const mapRef = useRef<MapRef | null>(null);
+  const onSelectLagoon = useCallback(
+  ({ longitude, latitude, zoom }: { longitude: number; latitude: number; zoom:number }) => {
+    mapRef.current?.getMap().flyTo({
+      center: [longitude, latitude],
+      zoom,
+      duration: 10000
+    });
+  },
+  []
+);
   return (
-  <div className="h-full w-full grid grid-rows-5 font-hand pointer-events-auto overflow-hidden">
-    <div className="row-start-3 row-span-3 border-t-4 border-white">
-      <LagoonMap/>
+  <div className="h-full w-full grid grid-rows-6 font-hand pointer-events-auto overflow-hidden">
+    <div className="row-span-2">
+
     </div>
-    <div className="absolute-center-x bottom-20">
+    <div className="row-start-3 flex items-end">
+      <ControlPanel onSelectLagoon={onSelectLagoon}/>
+    </div>
+    <div className="row-start-4 row-span-3 border-8 border-black/40 p-7 text-transparent bg-[url('/assets/bg.png')] bg-cover bg-center">
+    <div className="h-full w-full border-4 border-white">
+      <Map
+      ref={mapRef}
+      dragPan={true}
+      scrollZoom={true}
+      initialViewState={initialViewState}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+      dragRotate={false}
+      doubleClickZoom={false}
+      touchZoomRotate={false}
+      keyboard={false}
+      />
+    </div>
+
+    </div>
+    <div className="absolute-center-x bottom-5">
       <PageSwitcher
       currentPageId={pageIds.eu}
       className="max-w-[250px] min-w-[120px]"
